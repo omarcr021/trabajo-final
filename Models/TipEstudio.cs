122658398740fe7acbb3354f7c9051a8b7c17e45
Santiago Deza <santi_pro14@hotmail.com>
2026-04-24 20:24:09 -0500

    seccion tips

---
+++ Models/TipEstudio.cs
@@ -0,0 +1,10 @@
+namespace trabfinal.Models;
+
+public class TipEstudio
+{
+    public int Id { get; set; }
+    public string Titulo { get; set; } = string.Empty;
+    public string Categoria { get; set; } = string.Empty;
+    public string Descripcion { get; set; } = string.Empty;
+    public string AccionRecomendada { get; set; } = string.Empty;
+}
