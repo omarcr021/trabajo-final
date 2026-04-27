7bac8522feebbe0f5b32fb4681e53a4f1df97532
Santiago Deza <santi_pro14@hotmail.com>
2026-04-26 19:14:41 -0500

    redis y guardar tarear en bd

---
+++ Models/Tarea.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace trabfinal.Models;
+
+public class Tarea
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string Titulo { get; set; } = string.Empty;
+
+    public string? Materia { get; set; }
+
+    [Required]
+    public string Prioridad { get; set; } = "media";
+
+    public bool Completada { get; set; }
+
+    public DateTime FechaCreacion { get; set; } = DateTime.UtcNow;
+}
